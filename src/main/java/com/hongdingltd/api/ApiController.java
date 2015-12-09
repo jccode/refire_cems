7a2002ec9003a1396d9dfbd696d69f06d5b1ec5e
jessie <junchangchen@gmail.com>
2015-12-09 09:47:45 +0800

    upload

--- src/main/java/com/hongdingltd/api/ApiController.java
+++ src/main/java/com/hongdingltd/api/ApiController.java
@@ -1,9 +1,11 @@
 package com.hongdingltd.api;
 
 import com.hongdingltd.core.domain.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by jcchen on 15-12-6.
@@ -18,4 +20,29 @@ public class ApiController {
         return null;
     }
 
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public @ResponseBody
+    String handleFormUpload(@RequestParam("name") String name, @RequestParam("file")MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+
+                String dir = "/home/jcchen/temp/upload/";
+
+//                byte[] bytes = file.getBytes();
+//                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(dir+name)));
+//                stream.write(bytes);
+//                stream.close();
+
+                File f = new File(dir+name);
+                System.out.println(f.getAbsolutePath());
+                file.transferTo(f);
+                return "You successfully uploaded "+name+"!";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "You failed to upload " + name + " => "+e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because file was empty.";
+        }
+    }
 }
